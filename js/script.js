b979cd8cbdaeb733c7b52d51300839c9c0a65c89
Rina Schiller <schiller.rina@gmail.com>
2020-03-16 14:32:44 -0400

    changed api call to secure link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 DATA1 = document.getElementById('data1');
 
 $.ajax({   type: "GET",
-                url: "http://constitute.tech/tweets/?format=json&limit=100&politician__last_name=Ocasio-Cortez",
+                url: "https://constitute.herokuapp.com/tweets/?format=json&limit=100&politician__last_name=Ocasio-Cortez",
                 dataType: "json",
                 success: function (result, status, xhr) {
                     let data = result.results;
