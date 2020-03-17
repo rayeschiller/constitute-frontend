af220a9e9b15c7d38aff6b8d4f80337b182bc112
tom-shanahan <42326754+tom-shanahan@users.noreply.github.com>
2020-03-17 09:52:20 -0700

    rough bernie and warren

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 DATA1 = document.getElementById('data1');
+DATA2 = document.getElementById('data2');
 
 $.ajax({   type: "GET",
                 url: "https://constitute.herokuapp.com/tweets/?format=json&limit=10&politician__last_name=Ocasio-Cortez",
@@ -34,6 +35,56 @@ $.ajax({   type: "GET",
                 },
                 error: function (xhr, status, error) {
                     console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+                }
+            });
+
+$.ajax({   type: "GET",
+                url: "https://constitute.herokuapp.com/tweets/?format=json&limit=10&politician__last_name=Warren",
+                dataType: "json",
+                success: function (result, status, xhr) {
+                    $.ajax({   type: "GET",
+                        url: "https://constitute.herokuapp.com/tweets/?format=json&limit=10&politician__last_name=Sanders",
+                        dataType: "json",
+                        success: function (sanders_results, status, xhr) {
+                            let warren_data = result.results;
+                            console.log(result);
+                            let warren_toxicity = [];
+                            let warren_dates = [];
+                            let warren_texts = [];
+                            for (var i=0; i<warren_data.length; i++) {
+                                warren_toxicity.push(warren_data[i].toxicity);
+                                warren_dates.push(warren_data[i].date);
+                                warren_texts.push(warren_data[i].text);
+                            }
+                            console.log(warren_texts);
+
+                            let sanders_data = sanders_results.results;
+                            console.log(result);
+                            let sanders_toxicity = [];
+                            let sanders_dates = [];
+                            let sanders_texts = [];
+                            for (var i=0; i<sanders_data.length; i++) {
+                                sanders_toxicity.push(sanders_data[i].toxicity);
+                                sanders_dates.push(sanders_data[i].date);
+                                sanders_texts.push(sanders_data[i].text);
+                            }
+                            console.log(sanders_texts);
+                            const layout = {
+                                yaxis: {
+                                    range: [0, 1]
+                                },
+                                title: 'Sanders, Warren'
+                            };
+
+                            let sanders_toxicity_trace = create_trace(sanders_dates, sanders_toxicity, "Toxicity", sanders_texts);
+                            let warren_toxicity_trace = create_trace(warren_dates, warren_toxicity, "Toxicity", warren_texts);
+                            let trace_data = [warren_toxicity_trace,sanders_toxicity_trace];
+                            Plotly.newPlot( DATA2, trace_data, layout);
+                        },
+                        error: function (xhr, status, error) {
+                            console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+                        }
+                    });
                 }
             });
 
