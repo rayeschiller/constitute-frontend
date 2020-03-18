7dd2fa8c7f1dfc81f2e48baffe88972c51979c6f
tom-shanahan <42326754+tom-shanahan@users.noreply.github.com>
2020-03-18 09:03:45 -0700

    small change to plot

--- js/script.js
+++ js/script.js
@@ -41,37 +41,36 @@ $.ajax({   type: "GET",
 
 //TOMS CHART
 $.ajax({   type: "GET",
-                url: "https://constitute.herokuapp.com/tweets/?format=json&limit=2000&gender=Female",
+                url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Female",
                 dataType: "json",
-                success: function (result, status, xhr) {
+                success: function (female_results, status, xhr) {
                     $.ajax({   type: "GET",
-                        url: "https://constitute.herokuapp.com/tweets/?format=json&limit=2000&gender=Male",
+                        url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Male",
                         dataType: "json",
                         success: function (male_results, status, xhr) {
                             let female_explicit = [];
                             let female_dates = [];
                             let female_texts = [];
 
-                            let male_explicit = [];
-                            let male_dates = [];
-                            let male_texts = [];
-
-                            let female_data = result.results;
-
+                            let female_data = female_results.results;
                             for (var i=0; i<female_data.length; i++) {
                                 female_explicit.push(female_data[i].sexually_explicit);
                                 female_dates.push(female_data[i].date);
                                 female_texts.push(get_newline_text(female_data[i].text));
                             }
 
-                            let male_data = male_results.results;
+                            let male_explicit = [];
+                            let male_dates = [];
+                            let male_texts = [];
 
+                            let male_data = male_results.results;
                             for (var i=0; i<male_data.length; i++) {
                                 male_explicit.push(male_data[i].sexually_explicit);
                                 male_dates.push(male_data[i].date);
                                 male_texts.push(male_data[i].text);
                                 male_texts.push(get_newline_text(male_data[i].text));
                             }
+
                             const layout = {
                                 yaxis: {
                                     range: [0, 1]
@@ -80,9 +79,9 @@ $.ajax({   type: "GET",
                             };
 
                             let male_explicit_trace = gender_trace(male_dates, male_explicit, "Male Politicians", male_texts);
-                            let female_explicit_trace = gender_trace(male_dates, female_explicit, "Female Politicians", female_texts);
+                            let female_explicit_trace = gender_trace(female_dates, female_explicit, "Female Politicians", female_texts);
                             let trace_data = [female_explicit_trace,male_explicit_trace];
-                            Plotly.newPlot( DATA2, trace_data, layout);
+                            Plotly.newPlot(DATA2, trace_data, layout);
                         },
                         error: function (xhr, status, error) {
                             console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
