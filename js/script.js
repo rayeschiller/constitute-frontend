aeb5b2cb8d08a88d91871dab82e4b214ed6e2de6
Rina Schiller <schiller.rina@gmail.com>
2020-03-25 23:23:05 -0400

    added tweet chart

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ DATA1 = document.getElementById('data1');
 DATA2 = document.getElementById('data2');
 DATA3 = document.getElementById('data3');
 
-function create_chart(last_name, query_date) {
-//RINA's CHART 1
+function get_date_query(query_date){
     let gt_month = query_date.getMonth();
     let lt_month = query_date.getMonth()+2;
     let gt_year = query_date.getFullYear()-1;
@@ -11,11 +10,15 @@ function create_chart(last_name, query_date) {
     let gt_day = query_date.getDate()-1;
     let lt_day = query_date.getDate()+1;
     console.log("gt_month %s, lt_month %s, gt_year %s, lt_year %s, gt_day %s, lt_day %s", gt_month, lt_month, gt_year, lt_year, gt_day, lt_day);
-    console.log(query_date);
+    return "&date__year__lt=" + lt_year + "&date__year__gt=" + gt_year + "&date__month__gt=" + gt_month + "&date__month__lt=" + lt_month + "&date__day__gt=" + gt_day + "&date__day__lt=" + lt_day;
+};
+
+function create_tweet_chart(last_name, query_date) {
+    let q_date = get_date_query(query_date);
 
     $.ajax({
         type: "GET",
-        url: "https://constitute.herokuapp.com/tweets/?format=json&limit=100&politician__last_name=" + last_name + "&date__year__lt=" + lt_year + "&date__year__gt=" + gt_year + "&date__month__gt=" + gt_month + "&date__month__lt=" + lt_month + "&date__day__gt=" + gt_day + "&date__day__lt=" + lt_day,
+        url: "https://constitute.herokuapp.com/tweets/?format=json&limit=100&politician__last_name=" + last_name + q_date,
         dataType: "json",
         success: function (result, status, xhr) {
             console.log(this.url);
@@ -62,84 +65,92 @@ function create_chart(last_name, query_date) {
     });
 }
 
+function create_gender_chart(query_date){
+    let q_date = get_date_query(query_date);
+
+    $.ajax({   type: "GET",
+                    url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Female" + q_date,
+                    dataType: "json",
+                    success: function (female_results, status, xhr) {
+                        $.ajax({   type: "GET",
+                            url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Male" + q_date,
+                            dataType: "json",
+                            success: function (male_results, status, xhr) {
+                                let female_explicit = [];
+                                let female_dates = [];
+                                let female_texts = [];
+
+                                let female_data = female_results.results;
+                                for (var i=0; i<female_data.length; i++) {
+                                    female_explicit.push(female_data[i].sexually_explicit);
+                                    female_dates.push(female_data[i].date);
+                                    female_texts.push(get_newline_text(female_data[i].text));
+                                }
+
+                                let male_explicit = [];
+                                let male_dates = [];
+                                let male_texts = [];
+
+                                let male_data = male_results.results;
+                                for (var i=0; i<male_data.length; i++) {
+                                    male_explicit.push(male_data[i].sexually_explicit);
+                                    male_dates.push(male_data[i].date);
+                                    male_texts.push(get_newline_text(male_data[i].text));
+                                }
+
+                                const layout = {
+                                    yaxis: {
+                                        range: [0, 1]
+                                    },
+                                     xaxis: {
+                                        title: {
+                                          text: 'Date'
+                                        },
+                                      },
+                                      yaxis: {
+                                        title: {
+                                          text: 'Toxicity Level'
+                                        }
+                                      }
+                                };
+
+                                let male_explicit_trace = gender_trace(male_dates, male_explicit, "Male Politicians", male_texts);
+                                let female_explicit_trace = gender_trace(female_dates, female_explicit, "Female Politicians", female_texts);
+                                let trace_data = [female_explicit_trace, male_explicit_trace];
+                                Plotly.newPlot(DATA2, trace_data, layout);
+                            },
+                            error: function (xhr, status, error) {
+                                console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+                            }
+                        });
+                    }
+                });
+
+    }
 $(document).ready(function(){
-        //Make initial graph
+        //Make initial tweets graph
         let last_name = "Ocasio-Cortez";
         let today = new Date();
-        create_chart(last_name, today);
+        create_tweet_chart(last_name, today);
+        create_gender_chart(today);
         //Change graph depending on politician
         $('#button1').click(function(){
             last_name = $('#combo :selected').val();
-            console.log(last_name);
-            create_chart(last_name, today);
+            create_tweet_chart(last_name, today, DATA1);
+        });
+        //Change graph depending on date
+        $('#date_tweets').click(function(){
+            let date = $('#date_tweets_field').val();
+            create_tweet_chart(last_name, new Date(date))
         });
 
-        $('#dateb').click(function(){
-            let date = $('#datep').val();
+        $('#date_gender').click(function(){
+            let date = $('#date_gender_field').val();
             console.log(date);
-            create_chart(last_name, new Date(date))
+            create_gender_chart(new Date(date))
         });
-
 });
-//TOMS CHART
-$.ajax({   type: "GET",
-                url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Female",
-                dataType: "json",
-                success: function (female_results, status, xhr) {
-                    $.ajax({   type: "GET",
-                        url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Male",
-                        dataType: "json",
-                        success: function (male_results, status, xhr) {
-                            let female_explicit = [];
-                            let female_dates = [];
-                            let female_texts = [];
 
-                            let female_data = female_results.results;
-                            for (var i=0; i<female_data.length; i++) {
-                                female_explicit.push(female_data[i].sexually_explicit);
-                                female_dates.push(female_data[i].date);
-                                female_texts.push(get_newline_text(female_data[i].text));
-                            }
-
-                            let male_explicit = [];
-                            let male_dates = [];
-                            let male_texts = [];
-
-                            let male_data = male_results.results;
-                            for (var i=0; i<male_data.length; i++) {
-                                male_explicit.push(male_data[i].sexually_explicit);
-                                male_dates.push(male_data[i].date);
-                                male_texts.push(get_newline_text(male_data[i].text));
-                                male_texts.push(get_newline_text(male_data[i].text));
-                            }
-
-                            const layout = {
-                                yaxis: {
-                                    range: [0, 1]
-                                },
-                                 xaxis: {
-                                    title: {
-                                      text: 'Date'
-                                    },
-                                  },
-                                  yaxis: {
-                                    title: {
-                                      text: 'Toxicity Level'
-                                    }
-                                  }
-                            };
-
-                            let male_explicit_trace = gender_trace(male_dates, male_explicit, "Male Politicians", male_texts);
-                            let female_explicit_trace = gender_trace(male_dates, female_explicit, "Female Politicians", female_texts);
-                            let trace_data = [female_explicit_trace,male_explicit_trace];
-                            Plotly.newPlot(DATA2, trace_data, layout);
-                        },
-                        error: function (xhr, status, error) {
-                            console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-                        }
-                    });
-                }
-            });
 
 
 
@@ -183,5 +194,4 @@ function gender_trace(x_data, y_data, name_info, texts=null) {
 }
 
 
-//Rinas chart 2 test
 
