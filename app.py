33f96d2f30b9b1e170f0d340d5c2b92dac359d94
Rina Schiller <schiller.rina@gmail.com>
2020-04-23 02:01:12 -0400

    adding bar graph for toxic tweet frequency

--- app.py
+++ app.py
@@ -10,20 +10,31 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+# @app.callback(dash.dependencies.Output('display-value', 'children'),
+#               [dash.dependencies.Input('dropdown', 'value')])
+# def display_value(value):
+#     return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- js/script.js
+++ js/script.js
@@ -65,74 +65,36 @@ function create_tweet_chart(last_name, query_date) {
     });
 }
 
-function create_gender_chart(query_date){
-    let q_date = get_date_query(query_date);
 
-    $.ajax({   type: "GET",
-                    url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Female" + q_date,
-                    dataType: "json",
-                    success: function (female_results, status, xhr) {
-                        $.ajax({   type: "GET",
-                            url: "https://constitute.herokuapp.com/tweets/?format=json&politician__gender=Male" + q_date,
-                            dataType: "json",
-                            success: function (male_results, status, xhr) {
-                                let female_explicit = [];
-                                let female_dates = [];
-                                let female_texts = [];
-
-                                let female_data = female_results.results;
-                                for (var i=0; i<female_data.length; i++) {
-                                    female_explicit.push(female_data[i].sexually_explicit);
-                                    female_dates.push(female_data[i].date);
-                                    female_texts.push(get_newline_text(female_data[i].text));
-                                }
-
-                                let male_explicit = [];
-                                let male_dates = [];
-                                let male_texts = [];
-
-                                let male_data = male_results.results;
-                                for (var i=0; i<male_data.length; i++) {
-                                    male_explicit.push(male_data[i].sexually_explicit);
-                                    male_dates.push(male_data[i].date);
-                                    male_texts.push(get_newline_text(male_data[i].text));
-                                }
-
-                                const layout = {
-                                    yaxis: {
-                                        range: [0, 1]
-                                    },
-                                     xaxis: {
-                                        title: {
-                                          text: 'Date'
-                                        },
-                                      },
-                                      yaxis: {
-                                        title: {
-                                          text: 'Toxicity Level'
-                                        }
-                                      }
-                                };
-
-                                let male_explicit_trace = gender_trace(male_dates, male_explicit, "Male Politicians", male_texts);
-                                let female_explicit_trace = gender_trace(female_dates, female_explicit, "Female Politicians", female_texts);
-                                let trace_data = [female_explicit_trace, male_explicit_trace];
-                                Plotly.newPlot(DATA2, trace_data, layout);
-                            },
-                            error: function (xhr, status, error) {
-                                console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-                            }
-                        });
-                    }
-                });
-
+function toxicity_frequency(){
+     $.ajax({
+         type: "GET",
+         url: "https://constitute.herokuapp.com/politicians/toxicity_counts?format=json&",
+         dataType: "json",
+         success: function (result, status, xhr) {
+             console.log(result);
+             var data = [
+                 {
+                     x: Object.keys(result),
+                     y: Object.values(result),
+                     type: 'bar'
+                 }
+             ];
+             Plotly.newPlot(DATA2, data);
+         },
+         error: function(xhr, status, error){
+            console.log("Error: " + error);
     }
+     });
+}
+
 $(document).ready(function(){
         //Make initial tweets graph
         let last_name = "Ocasio-Cortez";
         let today = new Date();
         create_tweet_chart(last_name, today);
-        create_gender_chart(today);
+        toxicity_frequency();
+        // create_gender_chart(today);
         //Change graph depending on politician
         $('#button1').click(function(){
             last_name = $('#combo :selected').val();
@@ -144,11 +106,11 @@ $(document).ready(function(){
             create_tweet_chart(last_name, new Date(date))
         });
 
-        $('#date_gender').click(function(){
-            let date = $('#date_gender_field').val();
-            console.log(date);
-            create_gender_chart(new Date(date))
-        });
+        // $('#date_gender').click(function(){
+        //     let date = $('#date_gender_field').val();
+        //     console.log(date);
+        //     create_gender_chart(new Date(date))
+        // });
 });
 
 
